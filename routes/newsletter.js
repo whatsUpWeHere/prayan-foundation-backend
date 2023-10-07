ee37d5817c4ec2cf4ed851dde1d734df20f42130
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2023-10-07 21:43:46 +0530

    modified the newsletter

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -6,16 +6,27 @@ const Newsletter = require("../models/newsletter.js");
 router.post("/subscribe", async (req, res) => {
     try {
 
-        const response = await Newsletter.create({
-            email: req.body.email
+        let email = await Newsletter.findOne({
+            email: req.body.email,
         });
+        if (!email) {
 
 
+            const response = await Newsletter.create({
+                email: req.body.email
+            });
 
-        res.status(201).json({
-            message: '${req.body.email} added successfully...',
-            response,
-        });
+            res.status(201).json({
+                message: '${req.body.email} added successfully...',
+                response,
+            });
+        }
+        else {
+            res.status(200).json({
+                message: `${req.body.email} already exists...`,
+            });
+        }
+
     } catch (error) {
         console.log(error.message);
         res.status(500).json({ message: "Error adding email...", error });
